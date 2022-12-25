2baed1cb44441ba3314f7ad9465d9da46ed1be9f
hrisa22 <hrisa_98@yahoo.com>
2022-12-25 12:29:44 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var r = prompt("please enter number r:") ;
+var H = prompt("please enter number H:") ;
+const PI = 3.14 ;
+
+var V = parseInt (r) * parseInt (r) * PI * parseInt (H) ;
+alert ("V is: " + V)
